d5d8282e224fa2709121d4b7cfd3299e301892b3
Muslim-Rana <muslim.inaam.rana@gmail.com>
2024-08-15 14:07:14 -0400

    Connected the frontend to the backend API to display all users in the database on the homepage UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import { Container, Stack, Text } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import UserGrid from "./components/UserGrid";
+import { useState } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+
 
   return (
     <Stack minH={"100vh"}>
-      <Navbar/>
+      <Navbar setUsers={setUsers}/>
 
       <Container maxW={"1200px"} my={4}>
         <Text 
@@ -22,7 +25,7 @@ function App() {
         </Text>
 
         
-        <UserGrid/>
+        <UserGrid users={users} setUsers={setUsers}/>
 
       </Container>
     </Stack>
